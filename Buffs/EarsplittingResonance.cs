a296a4c6ebeea8d4976c1d85ad7d3d0fee621913
Kamiyo <kamiyo.k91@gmail.com>
2022-10-30 21:41:37 +0100

    Update

--- Buffs/EarsplittingResonance.cs
+++ Buffs/EarsplittingResonance.cs
@@ -17,7 +17,7 @@ namespace TheWhiteNoiseProject.Buffs
         public override string BufName => "Earsplitting Pitch";
 
         public override string bufActivatedText =>
-            "Take 2~3 Bonus damage whenever The White Noise Damages this opponent. If the opponent has two or more Endured and/or Ineffective health defenses, whenever this character is Hit by an offensive die, they take damage equal to the half of the natural roll of that die instead. ";
+            _intesify ? "Take 2~3 Bonus damage whenever The White Noise Damages this opponent." : "Whenever this character is Hit by an offensive die, take damage equal to the half of the natural roll of that die. ";
 
         protected override string keywordIconId => "WhiteNoise2_md5488";
         public override int paramInBufDesc => 0;
