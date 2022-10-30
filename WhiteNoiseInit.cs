3e14c3195da95f75de4fb55c51c6080966ac4534
Kamiyo <kamiyo.k91@gmail.com>
2022-10-30 15:37:24 +0100

    Init Skin Update

--- WhiteNoiseInit.cs
+++ WhiteNoiseInit.cs
@@ -6,6 +6,7 @@ using BigDLL4221.Enum;
 using BigDLL4221.Models;
 using BigDLL4221.Utils;
 using LOR_DiceSystem;
+using MonoMod.Utils;
 
 namespace TheWhiteNoiseProject
 {
@@ -33,6 +34,7 @@ namespace TheWhiteNoiseProject
                 Uri.UnescapeDataString(new UriBuilder(Assembly.GetExecutingAssembly().CodeBase).Path));
             ModParameters.Path.Add(WhiteNoiseModParameters.PackageId, WhiteNoiseModParameters.Path);
             OnInitSprites();
+            OnInitSkins();
             OnInitKeypages();
             OnInitCards();
             OnInitPassives();
@@ -75,29 +77,34 @@ namespace TheWhiteNoiseProject
             ModParameters.KeypageOptions.Add(WhiteNoiseModParameters.PackageId, new List<KeypageOptions>
             {
                 new KeypageOptions(10000001, isDeckFixed: true, everyoneCanEquip: true,
-                    bookCustomOptions: new BookCustomOptions("Roland", originalSkin: "TheWhiteNoise_md5488",
-                        motionSounds: new Dictionary<MotionDetail, MotionSound>
-                        {
-                            { MotionDetail.S1, new MotionSound("Chainsaw1.wav") },
-                            { MotionDetail.S2, new MotionSound("Chainsaw2.wav") },
-                            { MotionDetail.S3, new MotionSound("ChainsawBlock.wav") },
-                            { MotionDetail.Z, new MotionSound("QueenBee_Queen_Strongt.wav") },
-                            { MotionDetail.G, new MotionSound("Roland_Guard", isBaseSoundWin: true) },
-                            { MotionDetail.H, new MotionSound("Roland_Mace", isBaseSoundWin: true) },
-                            { MotionDetail.S4, new MotionSound("RevolverShot.ogg") },
-                            { MotionDetail.S5, new MotionSound("RevolverShot.ogg") },
-                            { MotionDetail.F, new MotionSound("SniperShot.ogg") },
-                            { MotionDetail.S6, new MotionSound("Roland_Duralandal_Down", isBaseSoundWin: true) },
-                            { MotionDetail.S7, new MotionSound("Roland_Duralandal_Up", isBaseSoundWin: true) },
-                            { MotionDetail.S11, new MotionSound("Blue_Argalria_Strong_Atk1", isBaseSoundWin: true) },
-                            { MotionDetail.S12, new MotionSound("Blue_Argalria_Strong_Atk2", isBaseSoundWin: true) },
-                            { MotionDetail.S13, new MotionSound("StigmaMace.wav") },
-                            { MotionDetail.J, new MotionSound("StigmaAxe.wav") },
-                            { MotionDetail.S14, new MotionSound("StigmaDual.wav") }
-                        }))
+                    bookCustomOptions: new BookCustomOptions("Roland"))
             });
         }
-
+        private static void OnInitSkins()
+        {
+            ModParameters.SkinOptions.AddRange(new Dictionary<string, SkinOptions>
+            {
+                {"TheWhiteNoise_md5488",new SkinOptions(packageId:WhiteNoiseModParameters.PackageId,motionSounds:new Dictionary<MotionDetail, MotionSound>
+                {
+                    { MotionDetail.S1, new MotionSound("Chainsaw1.wav") },
+                    { MotionDetail.S2, new MotionSound("Chainsaw2.wav") },
+                    { MotionDetail.S3, new MotionSound("ChainsawBlock.wav") },
+                    { MotionDetail.Z, new MotionSound("Roland_DuelSword_Strong", isBaseSoundWin: true) },
+                    { MotionDetail.G, new MotionSound("Roland_Guard", isBaseSoundWin: true) },
+                    { MotionDetail.H, new MotionSound("Roland_Mace", isBaseSoundWin: true) },
+                    { MotionDetail.S4, new MotionSound("RevolverShot.ogg") },
+                    { MotionDetail.S5, new MotionSound("RevolverShot.ogg") },
+                    { MotionDetail.F, new MotionSound("SniperShot.ogg") },
+                    { MotionDetail.S6, new MotionSound("Roland_Duralandal_Down", isBaseSoundWin: true) },
+                    { MotionDetail.S7, new MotionSound("Roland_Duralandal_Up", isBaseSoundWin: true) },
+                    { MotionDetail.S11, new MotionSound("Blue_Argalria_Strong_Atk1", isBaseSoundWin: true) },
+                    { MotionDetail.S12, new MotionSound("Blue_Argalria_Strong_Atk2", isBaseSoundWin: true) },
+                    { MotionDetail.S13, new MotionSound("StigmaMace.wav") },
+                    { MotionDetail.J, new MotionSound("StigmaAxe.wav") },
+                    { MotionDetail.S14, new MotionSound("StigmaDual.wav") }
+                })}
+            });
+        }
         private static void OnInitCredenza()
         {
             ModParameters.CredenzaOptions.Add(WhiteNoiseModParameters.PackageId,
