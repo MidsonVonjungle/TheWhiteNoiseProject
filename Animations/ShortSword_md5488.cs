f04c31b015a5e674c31790395cfa7cf4fbdbf1bf
Midson <bruno_campos2012@outlook.com>
2022-10-30 02:10:39 -0300

    Added yet another animation

---
+++ Animations/ShortSword_md5488.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+public class BehaviourAction_BlackSilence_ShortSword_md5488 : BehaviourActionBase
+{
+    public override List<RencounterManager.MovingAction> GetMovingAction(
+      ref RencounterManager.ActionAfterBehaviour self,
+      ref RencounterManager.ActionAfterBehaviour opponent)
+    {
+        bool flag = false;
+        if (opponent.behaviourResultData != null)
+            flag = opponent.behaviourResultData.IsFarAtk();
+        if (self.result != Result.Win || self.data.actionType != ActionType.Atk || flag)
+            return base.GetMovingAction(ref self, ref opponent);
+        List<RencounterManager.MovingAction> movingAction1 = new List<RencounterManager.MovingAction>();
+        RencounterManager.MovingAction movingAction2 = new RencounterManager.MovingAction(ActionDetail.S10, CharMoveState.MoveForward, 30f, false, 0.9f);
+        movingAction2.customEffectRes = "FX_PC_RolRang_Dagger";
+        movingAction2.SetEffectTiming(EffectTiming.PRE, EffectTiming.PRE, EffectTiming.PRE);
+        new RencounterManager.MovingAction(ActionDetail.S7, CharMoveState.Stop, 0.0f, delay: 0.1f).SetEffectTiming(EffectTiming.NOT_PRINT, EffectTiming.NOT_PRINT, EffectTiming.NOT_PRINT);
+        movingAction1.Add(movingAction2);
+        opponent.infoList.Add(new RencounterManager.MovingAction(ActionDetail.Damaged, CharMoveState.Stop, updateDir: false, delay: 0.5f));
+        return movingAction1;
+    }
+}
