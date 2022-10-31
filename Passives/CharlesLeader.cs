ff2a7cbe4bf0c5b6f40a3be86987bdc6ca818d20
Midson <bruno_campos2012@outlook.com>
2022-10-31 13:47:35 -0300

    Created a new passive

---
+++ Passives/CharlesLeader.cs
@@ -0,0 +1,29 @@
+﻿using BigDLL4221.Passives;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWhiteNoiseProject.Passives
+{
+    public class PassiveAbility_Charles_Leader_md5488 : PassiveAbility_PlayerMechBase_DLL4221
+    {
+        public override void OnRoundEnd()
+        {
+            if (this.owner.emotionDetail.EmotionLevel < 3)
+                return;
+            { this.owner.cardSlotDetail.RecoverPlayPoint(2); }
+        }
+        public override void BeforeRollDice(BattleDiceBehavior behavior)
+        {
+            int num1 = 0;
+            if (this.owner.emotionDetail.EmotionLevel >= 3)
+                num1 = 2;
+            behavior.ApplyDiceStatBonus(new DiceStatBonus()
+            {
+                min = num1,
+            });
+        }
+    }
+}
