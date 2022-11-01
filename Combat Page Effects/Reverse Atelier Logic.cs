e2d7fcd94fa63431cdd57a3fdd57229d62c3947d
Midson <bruno_campos2012@outlook.com>
2022-11-01 20:25:03 -0300

    Made it so that the cost on Cylinder cannot be altered

--- Combat Page Effects/Reverse Atelier Logic.cs
+++ Combat Page Effects/Reverse Atelier Logic.cs
@@ -2,12 +2,13 @@
 {
     public class DiceCardSelfAbility_Reverse_Atelier_Logic_md5488 : DiceCardSelfAbility_WhiteNoiseBaseCard_md5488
     {
-        public static string Desc = "[On Use] Draw 1 Page";
+        public static string Desc = "The Cost of this page cannot be changed [On Use] Draw 1 Page";
 
         public override void OnUseCard()
         {
             owner.allyCardDetail.DrawCards(1);
             base.OnUseCard();
         }
+        public override bool IsFixedCost() => true;
     }
 }
