b85b1d9530641008c84b23e930c3e729bb221cc1
Kamiyo <kamiyo.k91@gmail.com>
2022-11-03 12:20:23 +0100

    Hotfix for the DLL

--- Passives/Roland From Another World.cs
+++ Passives/Roland From Another World.cs
@@ -4,10 +4,10 @@ namespace TheWhiteNoiseProject.Passives
 {
     public class PassiveAbility_Roland_From_Another_World_md5488 : PassiveAbility_PlayerMechBase_DLL4221
     {
-        public override void OnWaveStart()
+        public override void Init(BattleUnitModel self)
         {
+            base.Init(self);
             SetUtil(new WhiteNoiseUtil().Util);
-            base.OnWaveStart();
         }
     }
 }
