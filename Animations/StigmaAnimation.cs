79cbca0a28d4b33a307e489e705a3c82a1741a95
Midson <bruno_campos2012@outlook.com>
2022-11-10 20:20:10 -0300

    animationsss

--- Animations/StigmaAnimation.cs
+++ Animations/StigmaAnimation.cs
@@ -12,7 +12,7 @@ public class BehaviourAction_StigmaAnim_md5488 : BehaviourActionBase
         if (self.result != Result.Win || self.data.actionType != ActionType.Atk || flag)
             return base.GetMovingAction(ref self, ref opponent);
         List<RencounterManager.MovingAction> movingAction1 = new List<RencounterManager.MovingAction>();
-        RencounterManager.MovingAction movingAction2 = new RencounterManager.MovingAction(ActionDetail.S5, CharMoveState.MoveForward, 30f, false, 0.9f);
+        RencounterManager.MovingAction movingAction2 = new RencounterManager.MovingAction(ActionDetail.S14, CharMoveState.MoveForward, 30f, false, 0.9f);
         movingAction2.SetEffectTiming(EffectTiming.PRE, EffectTiming.PRE, EffectTiming.PRE);
         new RencounterManager.MovingAction(ActionDetail.S14, CharMoveState.Stop, 0.0f, delay: 0.1f).SetEffectTiming(EffectTiming.NOT_PRINT, EffectTiming.NOT_PRINT, EffectTiming.NOT_PRINT);
         movingAction1.Add(movingAction2);
