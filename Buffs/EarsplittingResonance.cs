e3a9245dddb75509e87553870b8b36d33fd4ca18
Kamiyo <kamiyo.k91@gmail.com>
2022-12-02 19:00:55 +0100

    Hotfix

--- Buffs/EarsplittingResonance.cs
+++ Buffs/EarsplittingResonance.cs
@@ -13,13 +13,10 @@ namespace TheWhiteNoiseProject.Buffs
             stack = 0;
             _intesify = intesify;
         }
-
+        protected override string keywordId => _intesify
+            ? "EarsplittingResonance1_md5488"
+            : "EarsplittingResonance2_md5488";
         public override string BufName => "Earsplitting Pitch";
-
-        public override string bufActivatedText =>
-            _intesify
-                ? "Whenever this character is Hit by an offensive die, take damage equal to the half of the natural roll of that die"
-                : "Take 2~3 Bonus damage whenever The White Noise Damages this opponent";
 
         protected override string keywordIconId => "WhiteNoise2_md5488";
         public override int paramInBufDesc => 0;
@@ -37,8 +34,5 @@ namespace TheWhiteNoiseProject.Buffs
             if (!_intesify || atkDice == null) return;
             _owner.TakeDamage(atkDice.DiceVanillaValue / 2);
         }
-        protected override string keywordId => _intensify
-            ? "EarsplittingResonance1_md5488"
-            : "EarsplittingResonance2_md5488";
     }
 }
