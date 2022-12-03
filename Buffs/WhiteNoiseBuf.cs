4d94a96f8cac5f0e1c949f5e74f239b3531716e3
Midson <bruno_campos2012@outlook.com>
2022-12-02 21:30:57 -0300

    Fixing keywords

--- Buffs/WhiteNoiseBuf.cs
+++ Buffs/WhiteNoiseBuf.cs
@@ -10,9 +10,10 @@ namespace TheWhiteNoiseProject.Buffs
 
         public override string BufName => "White Noise";
 
-   //     public override string bufActivatedText =>
-   //         "With 3 stacks of White Noise, a special page can be played against this character to inflict an unique ailment on them";
+        //     public override string bufActivatedText =>
+        //         "With 3 stacks of White Noise, a special page can be played against this character to inflict an unique ailment on them";
 
+        protected override string keywordId => "WhiteNoiseBuff_md5488";
         protected override string keywordIconId => "WhiteNoise_md5488";
         public override int MaxStack => 3;
         public override bool DestroyedAt0Stack => true;
