6461e6d6b23c5f3bb8b0428a5f540c8412bacb1a
Kamiyo <kamiyo.k91@gmail.com>
2022-12-07 17:53:10 +0100

    Hotfix

--- WhiteNoiseInit.cs
+++ WhiteNoiseInit.cs
@@ -50,7 +50,7 @@ namespace TheWhiteNoiseProject
         {
             ModParameters.CustomBookSkinsOptions.Add(WhiteNoiseModParameters.PackageId, new List<CustomBookSkinsOption>
             {
-                new CustomBookSkinsOption("HeadlessWhiteNoise_md5488", 10000002,iconId: "IconNameHERE")
+                new CustomBookSkinsOption("HeadlessWhiteNoise_md5488", 10000002,iconId: "White_Noise_Icon_md5488")
             });
         }
         private static void OnInitSprites()
